dffd6fa1f86da95adce26ca58bf3bd3e8f530c57
Mateus Adamo <tecoadamo2003@gmail.com>
2023-08-19 05:42:31 -0300

    Aplicando FlatList

--- App.js
+++ App.js
@@ -1,20 +1,63 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { View, Text, StyleSheet, FlatList } from 'react-native';
 
-export default function App() {
+ class App extends Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      feed:[
+        {id: '1', nome: 'Mateus', idade: 19, email:'adamomateus478@gmail.com'},
+        {id: '2', nome: 'Marcos', idade: 29, email:'marcos78@gmail.com'},
+        {id: '3', nome: 'André', idade: 22, email:'andré48@gmail.com'},
+        {id: '4', nome: 'Lucas', idade: 31, email:'lucas28@gmail.com'},
+        {id: '5', nome: 'Pedro', idade: 27, email:'teco@gmail.com'},
+        {id: '6', nome: 'Luiz', idade: 45, email:'luiz@gmail.com'},
+      ]
+    }
+  }
+
+ render(){
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+
+        <FlatList
+        data={this.state.feed}
+        keyExtractor={(item) => item.id}
+        renderItem={ ({item}) => <Pessoa data={item}/> }
+        />
+
     </View>
-  );
+   );
+ }
 }
 
 const styles = StyleSheet.create({
-  container: {
+  container:{
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
-});
+  areaPessoa:{
+    backgroundColor: 'grey',
+    height: 200,
+    marginBottom: 15,
+    marginTop: 30,
+  },
+  textoPessoa:{
+    color: 'white',
+    fontSize: 20,
+  },
+})
+
+export default App;
+
+class Pessoa extends Component{
+  render(){
+    return(
+      <View style={styles.areaPessoa}>
+      <Text style={styles.textoPessoa}> Nome: {this.props.data.nome} </Text>
+      <Text style={styles.textoPessoa}> Idade: {this.props.data.idade} </Text>
+      <Text style={styles.textoPessoa}> E-mail: {this.props.data.email} </Text>
+      </View>
+    )
+  }
+}
